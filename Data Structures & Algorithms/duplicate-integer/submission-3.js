0ecad88f92ca2a9f2cf969ea40d9b81f961a086e
sandhya jaiswal <97659446+sandhhhya@users.noreply.github.com>
2026-04-24 09:12:15 -0700

    Add: duplicate-integer - submission-3

---
+++ Data Structures & Algorithms/duplicate-integer/submission-3.js
@@ -0,0 +1,14 @@
+class Solution {
+    hasDuplicate(nums) {
+        let seen = new Set();
+        
+        for (let num of nums) {
+            if (seen.has(num)) {
+                return true;
+            }
+            seen.add(num);
+        }
+        
+        return false;
+    }
+}
